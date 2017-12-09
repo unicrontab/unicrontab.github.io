ecf025917fb8f3eb64ded5ddb7503930ff54ae5b
Michael Wilson <michael.wilson@fishtech.group>
2017-12-09 00:31:22 -0600

    Adding my game jam games.

--- src/projectData.js
+++ src/projectData.js
@@ -56,6 +56,33 @@ export default [
             cybersecurity company logos from a JSON data array.`,
     },
     {
+        date: '7.0.2016',
+        name: 'Hoags Voyage',
+        image: 'https://gm48.net/storage/files/game/265/265_1468864116_icon_HoagGifFinal.gif',
+        url: 'https://gm48.net/game/265/hoag-s-voyage',
+        description: `Hoag the space explorer has crashed his ship on an
+            alien planet - help him design and build a new one while
+            fighting off enraged enemies. But beware...just because
+            you get your ship off the ground doesn't mean you're out
+            of trouble yet! Use your newly crafted vessel to destroy
+            all enemies and send him on his way to safety.`,
+    },
+    {
+        date: '10.0.2016',
+        name: 'Jungle Flow',
+        image: 'https://gm48.net/storage/files/game/327/327_1476663185_banner_BANNER.PNG',
+        url: 'https://gm48.net/game/327/jungle-flow',
+        description: ` It's a jungle out there! And when you're a bird
+        looking for your next meal, you'd better stay on top of your game.
+        Jungle Flow is a fun experience that requires quick thinking and
+        reflexes. Avoid the top, bottom, and ancient ruins as you fly through
+        the jungle's canopy...and be sure that your bird's color matches the
+        upcoming fruit! If you can keep up with avoiding the obstacles and
+        consistently obtaining the fruit, you'll be rewarded with multipliers
+        throughout your journey. Best of luck!
+        `,
+    },
+    {
         date: '04.07.14',
         name: 'Arduino Qwerty-To-Dvorak Converter',
         image: qwertydvorak,
@@ -80,4 +107,11 @@ export default [
         called it quits.
         `,
     },
+    {
+        date: '0?.0?.2014-ish',
+        name: 'StellarExile',
+        image: 'https://lh3.googleusercontent.com/g9ToNnqPgDSZSs0lQ9uxy2XkZFEbvGRdXxoxrgvXFlDZcR0z1S6ltd8AvUTjBeOEV142r6_ZNrhEbtWgCqfJ6z_FIGIFB_kse3HiWsjKY_ngL9zYQgkEFl3RPTtYC_8X2ylhGUkhIMFdKL_cjcPI8YKJo7gsuWXG_cK6bD78s8pb8VH7u-ZwT6o1mGk0mfJe4J2ws8qpJ9zvRp8jS2Ri296-BN58WDH7vzLG3sP_rOHAa5rYjghoUXF8ghSWGhdo8Dx4I83AcgIkXET4CgGkJrVqkqLh4e4n16kdAVpifcfOwQVRD2hS3G6Oi-Pfx49fQ-LYXeAQBgY-bTTZ0r-IGhLt-RuqQIIj3zIRKqQkccbSRmesKOUBTeTSoC4-Vt2VtqUEvOq7aOLoFYoJwRQPbGocI16LRoiI993Ga_iOZyX_K8bWD8bRpj6_sxIbrz0m7meYz-9kYNI_gmnL6UzTc2e3rnmT4JiSmFrG8ecvmdhPCnacJtYkTY42EVoMZrBXLcBG-jDobTSXENmZm0cOnwKicdywfwP4RAETNbGXfOTZs5H9ZOSxX9Pz9xIaomfrLhCUh-HPXD9Dz3bAizNpdLDACj3urF6OjedrwYHvdMsCdjmJRjm9pYl0auZmTYzIkETdDNy08KO4rQec3ihktKSYcTmghlvAwGc=w1802-h946-no',
+        url: 'https://photos.google.com/share/AF1QipPUDOlzNnvgPzf3fIOrEf2C_1i4aSZrKqSCjMhOceBhqckjlrO_gqU8QzxLA9ODcA/photo/AF1QipMpO5iSi-TYOzQKOfwLUwOrWO1LiBlL9JaxKvcE?key=THlxTExTREVkekhUUVdtNmlSVEdmUWVuaWx0UXp3',
+        description: `A 2.5D rpg/beat-em-up set in an alien landscape.`
+    }
 ];
